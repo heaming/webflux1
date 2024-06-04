2c1a98787a86db5549754c8fa10f68f0067dd29f
heaming <meetoohi@gmail.com>
2024-06-04 12:44:57 +0900

    [20240604] ReactiveRedis * RedisConfig.java * UserService/findById(), update()

---
+++ src/main/java/com/example/webflux1/config/RedisConfig.java
@@ -0,0 +1,62 @@
+package com.example.webflux1.config;
+
+import com.example.webflux1.repository.User;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+import java.time.Duration;
+
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+public class RedisConfig implements ApplicationListener<ApplicationReadyEvent> {
+
+// R2dbc처럼 lazy하게 연결 : connection 검증 x
+
+    private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        reactiveRedisTemplate.opsForValue().get("1")
+                .doOnSuccess(i -> log.info("Initialize to redis connection"))
+                .doOnError(err -> log.error("Failed to Initialize to redis connection: {}", err.getMessage()))
+                .subscribe();
+
+//        reactiveRedisTemplate.opsForList().leftPush("list1", "hello").subscribe();
+//        reactiveRedisTemplate.opsForValue().set("sampleKey1", "sample", Duration.ofSeconds(60)).subscribe();
+    }
+
+    @Bean
+    public ReactiveRedisTemplate<String, User> reactiveRedisUserTemplate(ReactiveRedisConnectionFactory connectionFactory) {
+
+        var objectMapper = new ObjectMapper()
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .registerModule(new JavaTimeModule())
+                .disable(SerializationFeature.WRITE_DATE_KEYS_AS_TIMESTAMPS);
+
+        Jackson2JsonRedisSerializer<User> jsonSerializer = new Jackson2JsonRedisSerializer<>(objectMapper, User.class);
+
+        RedisSerializationContext<String, User> serializationContext = RedisSerializationContext
+                .<String, User> newSerializationContext()
+                .key(RedisSerializer.string())
+                .value(jsonSerializer)
+                .hashKey(RedisSerializer.string())
+                .hashValue(jsonSerializer)
+                .build();
+
+        return new ReactiveRedisTemplate<>(connectionFactory, serializationContext);
+    }
+}
--- src/main/java/com/example/webflux1/service/UserService.java
+++ src/main/java/com/example/webflux1/service/UserService.java
@@ -4,9 +4,12 @@ import com.example.webflux1.repository.User;
 import com.example.webflux1.repository.UserR2dbcRepository;
 import com.example.webflux1.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +17,7 @@ public class UserService {
 
 //    private final UserRepository userRepository;
     private final UserR2dbcRepository userRepository;
+    private final ReactiveRedisTemplate<String, User> reactiveRedisTemplate;
 
     public Mono<User> create(String name, String email) {
         return userRepository.save(User.builder()
@@ -26,13 +30,28 @@ public class UserService {
         return userRepository.findAll();
     }
 
+    private String getUserCacheKey(Long id) {
+        return "users:%d".formatted(id);
+    }
     public Mono<User> findById(Long id) {
-        return userRepository.findById(id);
+        // 1. redis 조회
+        // 2. 값이 존재하면 응답
+        // 3. 없으면 db 질의 -> 결과 redis에 저장
+        return reactiveRedisTemplate.opsForValue()
+                .get(getUserCacheKey(id))
+                .switchIfEmpty(userRepository.findById(id)
+                        .flatMap(u -> reactiveRedisTemplate.opsForValue()
+                                .set(getUserCacheKey(id), u, Duration.ofSeconds(30))
+                                .then(Mono.just(u))
+                        )
+
+                ).log();
     }
 
-//    public Mono<Integer> deleteById(Long id) {
     public Mono<Void> deleteById(Long id) {
-        return userRepository.deleteById(id);
+        return userRepository.deleteById(id)
+                .then(reactiveRedisTemplate.unlink(getUserCacheKey(id)))
+                .then(Mono.empty());
     }
 
     public Mono<Void> deleteByName(String name) {
@@ -45,6 +64,8 @@ public class UserService {
                     u.setName(name);
                     u.setEmail(email);
                     return userRepository.save(u);
-                });
+                })
+                .flatMap(u -> reactiveRedisTemplate.unlink(getUserCacheKey(id))
+                        .then(Mono.just(u)));
     }
 }
